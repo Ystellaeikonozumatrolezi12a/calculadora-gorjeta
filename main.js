679d00af78efd2c642dda078c00a459b66480373
ystellaeii <ystellaeii@gmail.com>
2025-04-01 23:51:51 +0000

    recebendo a porcentagem da gorjeta ao clicar em qualquer um dos botões

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 let bill = 0;
 let tipPercentage = 0;
 let numberOfPeople = 0;
+let buttonSelected = null;
 
 function receiveBillValue() {
-    bill = document.querySelector("bill").valueAsNumber
+    bill = document.querySelector("#bill").valueAsNumber
 }
 
 function receiveNumberOfPeopleValue() {
-    numberOfPeople = document.querySelector("people").valueAsNumber
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercentageValue(value) {
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+    }
+    
+    tipPercentage = value / 100
+
+    buttonSelected = document.querySelector('#button-${value}')
+    buttonSelected.classList.add("button-selected")
 }
